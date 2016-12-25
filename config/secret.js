6b999681547c68ed6caae7ca6dd926c06bdced6b
Ahmed Salah <a.salah.b@gmail.com>
2016-12-25 18:04:49 +0200

    Facebook Link Updated

--- config/secret.js
+++ config/secret.js
@@ -8,7 +8,7 @@ module.exports = {
         clientID: process.env.FACEBOOK_ID || '1367776379933605',
         clientSecret: process.env.FACEBOOK_SECRET || '386ae53244f2d08150002dc100ff5e59',
         profileFields: ['emails', 'displayName'],
-        callbackURL: 'http://https://ecommerce-nodejs.herokuapp.com/auth/facebook/callback'
+        callbackURL: 'https://ecommerce-nodejs.herokuapp.com/auth/facebook/callback'
     },
     host: "https://ecommerce-nodejs.herokuapp.com/"
 }
